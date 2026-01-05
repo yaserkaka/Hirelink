d663ce5671e9c70cd7363d8d9c2b58fb5578bccf
Muhannad Elbolaky <think-basin-amber@duck.com>
2026-01-05 03:18:05 +0200

    📝 docs(email): add email transport documentation
    - Added documentation for the email transport wrapper.
    - Included notes on generating an app password for the Gmail user.
    - Documented the sendEmail function parameters and return value.

--- src/lib/email.js
+++ src/lib/email.js
@@ -1,21 +1,39 @@
+/**
+ * Email transport wrapper.
+ *
+ * Configures Nodemailer and exposes a helper to send emails.
+ *
+ * Notes:
+ * - You need to generate an app password for the GMAIL_USER in order to use this.
+ *
+ * References:
+ * - Nodemailer: https://nodemailer.com/
+ */
+
 import nodemailer from "nodemailer";
+import env from "../config/env.js";
 import errorUtils from "../utils/error.utils.js";
 import logger from "./logger.js";
 
 const transporter = nodemailer.createTransport({
-	host: process.env.GMAIL_HOST,
+	host: env.GMAIL_HOST,
 	port: 465,
 	secure: true,
 	auth: {
-		user: process.env.GMAIL_USER,
-		pass: process.env.GMAIL_PASSWORD,
+		user: env.GMAIL_USER,
+		pass: env.GMAIL_PASSWORD,
 	},
 });
 
+/**
+ * Sends an email using the configured Nodemailer transport.
+ * @param {{ to: string, subject: string, text?: string, html?: string }} params Parameters.
+ * @returns {Promise<boolean>} whether the email was sent successfully
+ */
 export default async function sendEmail({ to, subject, text, html }) {
 	const [err, _data] = await errorUtils(
 		transporter.sendMail({
-			from: process.env.GMAIL_USER,
+			from: env.GMAIL_USER,
 			to,
 			subject,
 			text,
