69d2be312d71998cc2debc707852af1d10d93fb7
Muhannad H. <captamari@proton.me>
2026-01-10 15:36:21 +0200

    📝 docs(error.utils): add error utils documentation

--- src/utils/error.utils.js
+++ src/utils/error.utils.js
@@ -1,3 +1,20 @@
+/**
+ * Error utility.
+ *
+ * Wraps a promise and returns a tuple `[error, data]`.
+ *
+ * Reference:
+ * - Learnt it form Web Dev Simplified: https://youtu.be/AdmGHwvgaVs
+ *
+ * Notes:
+ * - This is a golang style error handling, I like it, so I use it in Javascript.
+ /
+
+ /*
+ * @template T
+ * @param {Promise<T>} promise
+ * @returns {Promise<[unknown, T | undefined]>}
+ */
 export default function errorUtils(promise) {
 	return promise
 		.then((data) => [undefined, data])
