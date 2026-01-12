f4d685b4a58c3ab72ded58f6e448f976a1d56c73
Muhannad H. <captamari@proton.me>
2026-01-12 17:16:33 +0200

    📝 docs(index): update service barrel export documentation
    - Clarified notes on service dependencies and responsibilities.
    - Added references to Node ESM modules.
    - Included new jobService and moderationService exports.

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,23 @@
+/**
+ * Service barrel export.
+ *
+ * Centralizes service imports/exports so controllers and other modules can
+ * import from a single place.
+ *
+ * Notes:
+ * - Services implement business rules and should not depend on Express.
+ * - Controllers should call services, then translate their results into HTTP responses.
+ *
+ * References:
+ * - Node ESM modules: https://nodejs.org/api/esm.html
+ */
+
+import * as applicationService from "./application.service.js";
 import * as authService from "./auth.service.js";
 import * as emailService from "./email.service.js";
 import * as employerService from "./employer.service.js";
+import * as jobService from "./job.service.js";
+import * as moderationService from "./moderation.service.js";
 import * as talentService from "./talent.service.js";
 import * as tokenService from "./token.service.js";
 import * as userService from "./user.service.js";
@@ -8,8 +25,11 @@ import * as verificationService from "./verification.service.js";
 
 export {
 	authService,
+	applicationService,
 	emailService,
 	employerService,
+	jobService,
+	moderationService,
 	talentService,
 	tokenService,
 	userService,
